93200f41025d6ff311e47c03b3fd067ff37ddf15
dev-ant <njsharry32@gmail.com>
2025-10-13 21:07:13 +0900

    feat: 게시글 작성 뷰 추가

---
+++ src/app/(main)/community/write/page.tsx
@@ -0,0 +1,9 @@
+import PostWriteView from "@/components/page/community/PostWriteView";
+
+export default function CommunityWritePage() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-50 via-white to-purple-50 py-8">
+      <PostWriteView />
+    </div>
+  );
+}
