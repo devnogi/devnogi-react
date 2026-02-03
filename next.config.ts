ae84165ab6e0e91b06e8008eb1e773c8952f3c8a
dev-ant <njsharry32@gmail.com>
2026-02-03 12:54:37 +0900

    fix: next.config.ts에서 rewrite 설정 제거

--- next.config.ts
+++ next.config.ts
@@ -7,14 +7,6 @@ const nextConfig: NextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "http://168.107.43.221:8080/dcs/api/:path*",
-      },
-    ];
-  },
 };
 
 export default nextConfig;
