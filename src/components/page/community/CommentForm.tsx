a5667db70bc566d0c99633d5fba1d430d6dd2428
dev-ant <njsharry32@gmail.com>
2026-02-09 20:30:58 +0900

    fix: 대댓글 생성 시 parentCommentId 전송하게 수정

--- src/components/page/community/CommentForm.tsx
+++ src/components/page/community/CommentForm.tsx
@@ -39,7 +39,7 @@ export default function CommentForm({
     try {
       await clientAxios.post(`/comments/${postId}`, {
         content: content.trim(),
-        parentCommentId: parentCommentId ?? null,
+        parentComment: parentCommentId ?? null,
       });
 
       setContent("");
